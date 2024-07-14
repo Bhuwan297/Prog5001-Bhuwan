075de465a40edc422505e7d2b78c29e7c39b302c
Bhuwan297 <66291102+Bhuwan297@users.noreply.github.com>
2024-07-14 10:17:41 +1000

    Add files via upload

---
+++ Assessment_code/Studentmark.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Studentmark here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Studentmark
+
+{
+    private numberOfStudents = 30;
+    public static void main (String [] args) {
+    }
+}
